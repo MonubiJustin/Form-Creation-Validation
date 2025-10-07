a9a63c903ea119d1de498dbf089ca4a9fd82b08c
MonubiJustin <monubijustin@gmail.com>
2025-10-07 13:06:48 +0300

    done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function (){
     // form and feedback selection
 
     const form = document.getElementById("registration-form");
